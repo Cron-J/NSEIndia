f4904eccad22f78fff3264c940bb2563bc140f57
akashbudhia <akash@Akashs-MacBook-Air.local>
2015-10-30 04:06:35 +0530

    Write to DB

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ express = require('express')
 , fs = require('fs')
 , request = require('request')
 , Zip = require('adm-zip')
+, pg = require('pg')
+, copyFrom = require('pg-copy-streams').from
 , app = express();
 
 app.get('/', function (req, res) {
@@ -17,40 +19,43 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
 format.extend(String.prototype);
+
 //eg.: DERIVATIVES/2015/OCT/fo07OCT2015bhav.csv.zip
 var bhavFileBaseURL = 'http://www.nseindia.com/content/historical/DERIVATIVES/{0}/{1}/fo{2}{1}{0}bhav.csv.zip';
-var bhavFileName = 'fo{2}{1}{0}bhav.csv.zip';
+var bhavFileName = 'fo{2}{1}{0}bhav.csv';
 var bhavFilesZipDir = "./tmp/bhavFilesZip/";
 var bhavFilesCsvDir = "./tmp/bhavFilesCsv/";
 var bhavFileRequestHeader = { "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
+// var conString = "postgres://username:password@localhost/database";
+var conString = "postgres://postgres:cronj123@localhost/stk";
+
 function copyToDB(fileName){
-	//file downloaded successfully
+	pg.connect(conString, function(err, client, done) {
+	  var stream = client.query(copyFrom('COPY bhav FROM STDIN CSV HEADER'));
+	  var fileStream = fs.createReadStream(bhavFilesCsvDir + fileName);
+	  fileStream.pipe(stream).on('finish', function(){
+		console.log("Data Imported to PG: ", fileName);	
+	  });
+	});
 }
-function extractFile(filePath, success){
-	var zip = new Zip(filePath);
+function extractFile(fileName, success){
+	var zip = new Zip(bhavFilesZipDir + fileName + ".zip");
 	zip.extractAllTo(bhavFilesCsvDir); 
-	console.log("CSV Extracted: ", filePath);
-	success();
+	console.log("CSV Extracted: ", fileName);
+	// success(fileName);
 }
-var ctr = 0;
 function importBhavFile(year, month, day){
-	// ctr += 5000;
-	// setTimeout(function(){
-		
-	// }, ctr);
 	var fileName = bhavFileName.format(year, month, day);
 	var fileURL = bhavFileBaseURL.format(year, month, day);
 	
     var r = request({ url: fileURL, headers: bhavFileRequestHeader });
 	r.on('response', function (resp) {
-	   // resp.headers 
 	   if(resp.statusCode == 200){
-		   r.pipe(fs.createWriteStream(bhavFilesZipDir + fileName))
+		   r.pipe(fs.createWriteStream(bhavFilesZipDir + fileName + ".zip"))
 			.on('close', function(){
 				console.log("ZIP Downloaded: ", fileName);	
-				extractFile(bhavFilesZipDir + fileName, copyToDB);
+				extractFile(fileName, copyToDB);
 			});
 		}
 	});
@@ -59,4 +64,5 @@ function importBhavFilesBetween(startDate, endDate){
 	for(var date = startDate; date.isBefore(endDate); date.add(1,'days'))
 		importBhavFile(date.format('YYYY'),date.format('MMM').toUpperCase(),date.format('DD'));
 }
-importBhavFilesBetween(moment('2015-10-02'), moment('2015-10-22'));
+importBhavFilesBetween(moment('2015-09-30'), moment('2015-10-29'));
+// importBhavFilesBetween(moment('2015-04-30'), moment('2015-10-29'));
